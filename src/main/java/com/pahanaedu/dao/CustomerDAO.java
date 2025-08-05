db658fe4c7f4a0055dff0970d46f54674d052f98
kavii519 <methminiamarasiri74@gmail.com>
2025-08-05 11:03:50 +0530

    Define CustomerDAO interface for customer CRUD operations

---
+++ src/main/java/com/pahanaedu/dao/CustomerDAO.java
@@ -0,0 +1,13 @@
+package com.pahanaedu.dao;
+
+import com.pahanaedu.model.Customer;
+import java.util.List;
+
+public interface CustomerDAO {
+    boolean addCustomer(Customer customer);
+    Customer getCustomerById(int id);
+    List<Customer> getAllCustomers();
+    boolean updateCustomer(Customer customer);
+    boolean deleteCustomer(int id);
+    int getCustomerCount();
+}
